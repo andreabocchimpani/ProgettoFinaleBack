50c420adf75d24f977a9e44ff49fc83042e9dec1
andreabocchimpani <andreabocchimpani@gmail.com>
2020-01-10 13:41:47 +0100

    Classe Mail, Database Context e Migrations

--- Models/ProgettoFinaleContext.cs
+++ Models/ProgettoFinaleContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 namespace ProgettoFinaleBack
 {
     public class ProgettoFinaleContext : DbContext
@@ -6,7 +7,7 @@ namespace ProgettoFinaleBack
         public DbSet<Mail> Mails { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=localhost;Database=ProgettoFinaleBack;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=pippo;Trusted_Connection=True;MultipleActiveResultSets=true");
         }
         
     }
