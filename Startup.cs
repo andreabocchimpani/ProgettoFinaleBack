bf75ddd8a3be165f5722f625656df666cdc87a0b
andreabocchimpani <andreabocchimpani@gmail.com>
2020-01-13 10:22:44 +0100

    Injection

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,8 @@ namespace ProgettoFinaleBack
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ProgettoFinaleContext>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +53,10 @@ namespace ProgettoFinaleBack
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "api",
+                    pattern: "api/{controller}/{id?}");
             });
         }
     }
